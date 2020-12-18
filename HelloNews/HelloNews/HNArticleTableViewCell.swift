d2350abf6bb7143e0a2cfdc9f824399390382d1d
Aditi3 <aditi.gvc@gmail.com>
2020-12-18 19:13:36 +0530

    Update Article cell to display Image

--- HelloNews/HelloNews/HNArticleTableViewCell.swift
+++ HelloNews/HelloNews/HNArticleTableViewCell.swift
@@ -7,28 +7,37 @@
 //
 
 import UIKit
+import Kingfisher
 
 class HNArticleTableViewCell: UITableViewCell {
     
     @IBOutlet var titleLabel: UILabel?
     @IBOutlet var categoryLabel: UILabel?
     @IBOutlet var articleImageView: UIImageView?
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        setupLayout()
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
+    }
+    
+    func setupLayout() {
+        titleLabel?.textAlignment = .center
+        categoryLabel?.textAlignment = .left
+        articleImageView?.contentMode = .scaleAspectFit
     }
     
     func setData(article: Article) {
         titleLabel?.text = article.title
         categoryLabel?.text = article.category.rawValue
-        articleImageView?.image = UIImage()
+        let url = URL(string: article.urlToImage)
+        articleImageView?.kf.setImage(with: url)
     }
-
+    
 }
